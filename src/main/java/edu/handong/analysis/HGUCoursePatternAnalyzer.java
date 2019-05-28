9ac855e5b108a0452f03719de632b197791be74f
leekim611 <47958294+leekim611@users.noreply.github.com>
2019-05-28 18:32:39 +0900

    set analysis == 1 // except Exception

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -46,7 +46,31 @@ public class HGUCoursePatternAnalyzer {
 			// output
 			
 			// analysis
-			
+			if (Integer.parseInt(analysis) == 1) {
+				try {
+					// when there are not enough arguments from CLI, it throws the NotEnoughArgmentException which must be defined by you.
+					if(args.length<2)
+						throw new NotEnoughArgumentException();
+				} catch (NotEnoughArgumentException e) {
+					System.out.println(e.getMessage());
+					System.exit(0);
+				}
+				
+				String dataPath = input; // csv file to be analyzed
+				String resultPath = output; // the file path where the results are saved.
+				ArrayList<String> lines = Utils.getLines(dataPath, true);
+				
+				students = loadStudentCourseRecords(lines);
+				
+				// To sort HashMap entries by key values so that we can save the results by student ids in ascending order.
+				Map<String, Student> sortedStudents = new TreeMap<String,Student>(students); 
+				
+				// Generate result lines to be saved.
+				ArrayList<String> linesToBeSaved = countNumberOfCoursesTakenInEachSemester(sortedStudents);
+				
+				// Write a file (named like the value of resultPath) with linesTobeSaved.
+				Utils.writeAFile(linesToBeSaved, resultPath);
+			}
 			// coursecode
 			if (Integer.parseInt(analysis) == 2) {
 				
